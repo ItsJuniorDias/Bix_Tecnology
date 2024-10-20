01bd6a78beb28956b114eddf1cfe58d9aa925712
AlexandreJr <juniordias_@live.com>
2024-10-20 10:37:40 -0300

    chore: add date for list

--- nextjs-tecnology/src/app/pages/List/List.tsx
+++ nextjs-tecnology/src/app/pages/List/List.tsx
@@ -1,76 +1,115 @@
 import React from "react";
+import { format } from "date-fns";
 
-import { Container, Content, Text, ListContent } from "./styles";
+import {
+  Container,
+  Content,
+  Text,
+  ListContent,
+  ListItemContent,
+  ListItemDate,
+  ListItemIndustry,
+  TruncatedH5,
+} from "./styles";
+
 import { mockJson } from "../../data";
 
-interface Item {
-  date: number;
-  amount: string;
-  transaction_type?: string;
-  currency?: string;
-  account?: string;
-  industry?: string;
-  state?: string;
-}
+const MAX_LENGTH = 15;
 
 export const List = () => {
-  const renferItrem = (item: Item): React.ReactNode => (
-    <li key={item.date}>
-      <h2>{item.amount}</h2>
-    </li>
-  );
+  // const renderItem = (item: Item): React.ReactNode => (
+  //   <li key={item.date}>
+  //     <h2>{item.amount}</h2>
+  //   </li>
+  // );
 
   return (
     <Container>
       <Content>
+        <Text>Conta</Text>
+        <Text>Industria</Text>
         <Text>Data</Text>
         <Text>Quantia</Text>
         <Text>Tipo de transação</Text>
-        <Text>Conta</Text>
         <Text>Moeda</Text>
-        <Text>Estad o</Text>
+        <Text>Estado</Text>
       </Content>
 
       <ListContent>
+        <ListItemContent>
+          <ul>
+            {mockJson.map((item, index) => {
+              const truncatedText =
+                item?.account?.length > MAX_LENGTH
+                  ? item?.account?.substring(0, MAX_LENGTH) + "..."
+                  : item?.account;
+
+              return (
+                <li key={index}>
+                  <TruncatedH5>{truncatedText}</TruncatedH5>
+                </li>
+              );
+            })}
+          </ul>
+        </ListItemContent>
+
+        <ListItemIndustry>
+          <ul>
+            {mockJson.map((item, index) => {
+              const truncatedText =
+                item?.industry?.length > MAX_LENGTH
+                  ? item?.industry?.substring(0, MAX_LENGTH) + "..."
+                  : item?.industry;
+
+              return (
+                <li key={index}>
+                  <TruncatedH5>{truncatedText}</TruncatedH5>
+                </li>
+              );
+            })}
+          </ul>
+        </ListItemIndustry>
+
+        <ListItemDate>
+          <ul>
+            {mockJson.map((item, index) => {
+              const timestampString = item.date;
+              const dateObject = new Date(Number(timestampString));
+
+              const converterNewDate = new Date(dateObject).toDateString();
+
+              return (
+                <li key={index}>
+                  <TruncatedH5>{converterNewDate}</TruncatedH5>
+                </li>
+              );
+            })}
+          </ul>
+        </ListItemDate>
+
         {/* <ul>
-          {mockJson.map((item, index) => (
-            <li key={index}>
-              <h5>{item.}</h5>
-            </li>
-          ))}
-        </ul> */}
-        ``
-        <ul>
-          {mockJson.map((item, index) => (
-            <li key={index}>
-              <h5>{item.industry}</h5>
-            </li>
-          ))}
-        </ul>
-        <ul>
           {mockJson.map((item, index) => (
             <li key={index}>
               <h5>{item.amount}</h5>
             </li>
           ))}
         </ul>
+
+        <ul>
+          {mockJson.map((item, index) => (
+            <li key={index}>
+              <h5>{item.state}</h5>
+            </li>
+          ))}
+        </ul>
+
         <ul>
           {mockJson.map((item, index) => (
             <li key={index}>
               <h5>{item.currency}</h5>
             </li>
           ))}
-        </ul>
-        <ul>
-          {mockJson.map((item, index) => (
-            <li key={index}>
-              {/* Safely access item properties */}
-              <h5>{item.currency}</h5>
-              {/* {item.transaction_type && <p>Type: {item.transaction_type}</p>} */}
-              {/* Render other properties as needed */}
-            </li>
-          ))}
-        </ul>
+        </ul> */}
       </ListContent>
     </Container>
   );
--- nextjs-tecnology/src/app/pages/List/styles.ts
+++ nextjs-tecnology/src/app/pages/List/styles.ts
@@ -2,13 +2,12 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   width: 90%;
-  height: 80svh;
-  background-color: #ffffff;
+  height: 100svh;
+  background-color: blue;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   margin-left: 330px;
   margin-top: 64px;
-  margin-bottom: 32px;
   padding: 24px;
   padding-right: 32px;
   border-radius: 16px;
@@ -17,14 +16,15 @@ export const Container = styled.div`
 
 export const Content = styled.li`
   width: 100%;
-  height: 32px;
-  background-color: red;
+  background-color: yellow;
   border-radius: 8px;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: space-around;
   padding-left: 16px;
   padding-right: 16px;
+  padding-top: 8px;
+  padding-bottom: 8px;
   align-items: center;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1), 0 6px 20px rgba(0, 0, 0, 0.1);
 `;
@@ -46,6 +46,35 @@ export const Text = styled.h5`
 `;
 
 export const ListContent = styled.div`
-  width: 100%,
-  height: 100svg;
+  width: 100%;
+  height: 100svh;
+  margin-top: 24px;
+  background-color: green;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const ListItemContent = styled.div`
+  margin-left: 24px;
+  background-color: yellow;
+  width: 144px;
+`;
+
+export const ListItemIndustry = styled.div`
+  margin-left: px;
+  background-color: pink;
+`;
+
+export const ListItemDate = styled.div`
+  /* margin-right: 64px; */
+  background-color: grey;
+  margin-left: 32px;
+`;
+
+export const TruncatedH5 = styled.h5`
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  width: 100%; /* Adjust width as needed */
+  color: #ddd;
 `;
